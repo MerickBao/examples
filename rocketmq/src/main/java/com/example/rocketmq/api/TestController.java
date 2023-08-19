ebcc2519811111210e678f9cf1878c9068dfc9b7
MerickBao <66370724+MerickBao@users.noreply.github.com>
2023-08-19 20:09:35 +0800

    Update TestController.java

--- rocketmq/src/main/java/com/example/rocketmq/api/TestController.java
+++ rocketmq/src/main/java/com/example/rocketmq/api/TestController.java
@@ -2,10 +2,7 @@ package com.example.rocketmq.api;
 
 import com.example.rocketmq.model.User;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
